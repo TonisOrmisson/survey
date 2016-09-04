428ac8ad41daf2e4d6af37f8f292f39fb118eb7e
Tõnis Ormisson <tonis@andmemasin.eu>
2016-09-04 16:58:33 +0300

    Add Change survey get-new-app action

--- api/Survey.php
+++ api/Survey.php
@@ -4,5 +4,6 @@ namespace andmemasin\survey\api;
 class Survey
 {
     /** The action name of the Get-new-id in SurveyHub */
-    const ACTION_GETNEWID = "app-type/index";
+    const ACTION_SURVEY_GETNEWID = "survey/get-new-id";
+
 }
