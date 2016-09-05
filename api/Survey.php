4f717e8f89d05fa544ab9d043ee44a449df1efc5
Tõnis Ormisson <tonis@andmemasin.eu>
2016-09-05 09:20:25 +0300

    Add Add set-status action to Survey

--- api/Survey.php
+++ api/Survey.php
@@ -6,4 +6,6 @@ class Survey
     /** The action name of the Get-new-id in SurveyHub */
     const ACTION_SURVEY_GETNEWID = "survey/get-new-id";
 
+    /** The action name of the Set-status in SurveyHub */
+    const ACTION_SURVEY_SETNEWSTATUS = "survey/set-status";
 }
