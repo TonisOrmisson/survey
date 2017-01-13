3bdf23f81e47438c2e570839aba6f507d1028c52
Tõnis Ormisson <tonis@andmemasin.eu>
2017-01-13 16:21:01 +0200

    DEV respondent actions

--- api/Survey.php
+++ api/Survey.php
@@ -6,15 +6,29 @@ class Survey
     /** The action name of the Get-new-key in SurveyHub */
     const ACTION_SURVEY_GETNEWKEY = "survey/get-new-key";
 
+    /** The action name of the Get-new-key in SurveyHub */
+    const ACTION_RESPONDENT_GETNEWKEY = "respondent/get-new-key";
+
     /** The action name of the Set-status in SurveyHub */
     const ACTION_SURVEY_SETNEWSTATUS = "survey/set-status";
+
+    /** The action name of the Set-status in SurveyHub */
+    const ACTION_RESPONDENT_SETNEWSTATUS = "respondent/set-status";
 
     /** The action name of the get-status in SurveyHub */
     const ACTION_SURVEY_GETNEWSTATUS = "survey/get-status";
 
+    /** The action name of the get-status in SurveyHub */
+    const ACTION_RESPONDENT_GETNEWSTATUS = "respondent/get-status";
+
+    /** The action name of the view respondent in SurveyHub */
+    const ACTION_RESPONDENT_GET = "respondent/view";
     /** The action name of the get-apps in SurveyHub */
     const ACTION_SURVEY_GETAPPS = "survey/get-apps";
 
     /** The action name of the get-satuses in SurveyHub */
     const ACTION_SURVEY_GETSTATUSES = "survey/get-statuses";
+
+    /** The action name of the get-satuses in SurveyHub */
+    const ACTION_RESPONDENT_GETSTATUSES = "respondent/get-statuses";
 }
