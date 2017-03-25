a1949c49a82188d512a65c4399a8f0f810c7f700
Tõnis Ormisson <tonis@andmemasin.eu>
2017-03-25 08:33:16 +0200

    Add RejectionType

---
+++ RejectionType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace andmemasin\survey;
+
+use Yii;
+use yii\base\Model;
+
+class RejectionType extends Model
+{
+    const TYPE_HARD_BOUNCE = 'hard';
+    const TYPE_SOFT_BOUNCE = 'soft';
+    const TYPE_COMPLAINT = 'complaint';
+    const TYPE_ANSWERED = 'answered';
+    const TYPE_OTHER = 'other';
+
+    /**
+     * @return RejectionType[]
+     */
+    public static function getModels(){
+        return [
+            self::TYPE_HARD_BOUNCE => [
+                'type' => self::TYPE_HARD_BOUNCE,
+                'name'=> Yii::t('app','Email hard bounce'),
+                'description' => Yii::t('app','Hard bounce'),
+            ],
+            self::TYPE_SOFT_BOUNCE => [
+                'type' => self::TYPE_SOFT_BOUNCE,
+                'name'=> Yii::t('app','Email soft bounce'),
+                'description' => Yii::t('app','Soft bounce'),
+            ],
+            self::TYPE_COMPLAINT => [
+                'type' => self::TYPE_COMPLAINT,
+                'name'=> Yii::t('app','Email spam complaint'),
+                'description' => Yii::t('app','Email spam complaint'),
+            ],
+            self::TYPE_ANSWERED => [
+                'type' => self::TYPE_ANSWERED,
+                'name'=> Yii::t('app','Answered already'),
+                'description' => Yii::t('app','Answered already'),
+            ],
+            self::TYPE_OTHER => [
+                'type' => self::TYPE_OTHER,
+                'name'=> Yii::t('app','Other'),
+                'description' => Yii::t('app','Other'),
+            ],
+
+        ];
+    }
+
+    /**
+     * @param $id
+     * @return RejectionType|array
+     */
+    public static function getById($id){
+        $models = self::getModels();
+        if(isset($models[$id])){
+            return new static($models[$id]);
+        }
+        return [];
+    }
+
+}
